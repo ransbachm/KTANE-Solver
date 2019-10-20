e5779ca9a3054629549b2681cd225c7a01335965
ransbachm <ransbachm@gmail.com>
2019-10-20 21:41:59 +0200

    Add support for Buttons
    Add indicators CAR FRK
    Add button for Button Window

--- src/main/java/host/progamernothd/ktanemanual/windows/MainWindow.java
+++ src/main/java/host/progamernothd/ktanemanual/windows/MainWindow.java
@@ -15,12 +15,19 @@ import javax.swing.text.Document;
 import javax.swing.JLabel;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
+import javax.swing.JPanel;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class MainWindow {
 
 	public JFrame frame;
 	public JTextField txtEptd;
 	public static String serialID;
+	public JComboBox<String> batteryComboBox;
+	public JCheckBox frkCheckbox;
+	public JCheckBox carCheckbox;
 
 	/**
 	 * Launch the application.
@@ -84,20 +91,38 @@ public class MainWindow {
 		txtEptd.setColumns(10);
 		
 		JLabel lblSerialId = new JLabel("Serial id");
+		
+		JPanel panel = new JPanel();
+		
+		JPanel panel_1 = new JPanel();
+		
+		JButton btnKnopf = new JButton("Knopf");
+		btnKnopf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ButtonWindow.main(null);
+			}
+		});
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(btnNewButton)
-							.addPreferredGap(ComponentPlacement.RELATED, 259, Short.MAX_VALUE)
-							.addComponent(txtEptd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap())
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
 							.addComponent(lblSerialId)
-							.addGap(36))))
+							.addGap(36))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())
+						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btnKnopf, GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.RELATED, 234, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(panel, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(txtEptd, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addContainerGap())))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -108,8 +133,39 @@ public class MainWindow {
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnNewButton)
 						.addComponent(txtEptd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(215, Short.MAX_VALUE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(18)
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnKnopf)))
+					.addContainerGap(120, Short.MAX_VALUE))
 		);
+		
+		JLabel lblBatteries = new JLabel("Batteries");
+		panel_1.add(lblBatteries);
+		
+		batteryComboBox = new JComboBox();
+		panel_1.add(batteryComboBox);
+		batteryComboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5"}));
+		
+		frkCheckbox = new JCheckBox("FRK");
+		panel.add(frkCheckbox);
+		
+		carCheckbox = new JCheckBox("CAR");
+		panel.add(carCheckbox);
 		frame.getContentPane().setLayout(groupLayout);
+	}
+	public JCheckBox getFrkCheckbox() {
+		return frkCheckbox;
+	}
+	public JCheckBox getCarCheckbox() {
+		return carCheckbox;
+	}
+	public JComboBox getBatteryComboBox() {
+		return batteryComboBox;
 	}
 }
